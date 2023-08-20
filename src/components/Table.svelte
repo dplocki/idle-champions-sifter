e27f94c79131e854563c1695a2251e4406a9d0b6
Ziomioslaw <dawid.plocki@gmail.com>
2023-08-20 17:08:35 +0200

    Implement sort per column on Table component

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,19 +1,50 @@
 <script lang="ts">
-    export let datasource: any[] = [];
+	interface IColumnState {
+		name: string;
+		sort: 'None' | 'ASC' | 'DESC';
+	}
+
+	export let datasource: any[] = [];
+	let columns: IColumnState[] = [];
+
+	$: {
+		if (datasource.length > 0) {
+			columns = Object.keys(datasource[0]).map((columnName) => ({
+				name: columnName,
+				sort: 'None'
+			}));
+		}
+	}
+
+	function columnClick(column: IColumnState) {
+		const columnName = column.name;
+
+		datasource = datasource.sort((a: any, b: any) => {
+			if (a[columnName] > b[columnName]) {
+				return 1;
+			} else if (a[columnName] < b[columnName]) {
+				return -1;
+			}
+
+			return 0;
+		});
+	}
 </script>
 
 <table>
-    <thead>
-    </thead>
-    <tbody>
-    {#each datasource as datum}
-        <tr>
-        {#each Object.values(datum) as value}
-            <td>{value}</td>
-        {/each}
-        </tr>
-    {/each}
-    </tbody>
-    <tfoot>
-    </tfoot>
+	<thead>
+		{#each columns as column}
+			<th on:click={() => columnClick(column)}>{column.name}</th>
+		{/each}
+	</thead>
+	<tbody>
+		{#each datasource as datum}
+			<tr>
+				{#each columns as column}
+					<td>{datum[column.name]}</td>
+				{/each}
+			</tr>
+		{/each}
+	</tbody>
+	<tfoot />
 </table>
