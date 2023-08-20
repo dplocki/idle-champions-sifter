c91e4c7150f01f770f41b7054346b180ceb68729
Ziomioslaw <dawid.plocki@gmail.com>
2023-08-20 19:00:23 +0200

    App component: add styles

--- src/app.svelte
+++ src/app.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-    import QueryTextBox from "./components/QueryTextBox.svelte";
-	import Table from "./components/Table.svelte";
+	import { onMount } from 'svelte';
+	import QueryTextBox from './components/QueryTextBox.svelte';
+	import Table from './components/Table.svelte';
 
-    let results: any[] = [];
+	let results: any[] = [];
 
-    function newquery(event: Event) {
-        console.log('newquery', event);
-    }
+	function newquery(event: Event) {
+		console.log('newquery', event);
+	}
 
-    onMount(async () => {
-        results = await fetch('./database.json').then(response => response.json())
-    });
-
+	onMount(async () => {
+		results = await fetch('./database.json').then((response) => response.json());
+	});
 </script>
 
 <h1>Idle Champions Sifter</h1>
@@ -20,3 +19,11 @@
 <QueryTextBox on:query={newquery} />
 
 <Table datasource={results} />
+
+<style>
+	h1 {
+		font-size: 24px;
+		color: #333;
+		margin-bottom: 10px;
+	}
+</style>
