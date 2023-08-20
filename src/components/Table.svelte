b4f2860f11df8266699330d7a577c0f14d42b130
Ziomioslaw <dawid.plocki@gmail.com>
2023-08-20 18:17:50 +0200

    Component Table can now sort in two-ways

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	interface IColumnState {
 		name: string;
 		sort: 'None' | 'ASC' | 'DESC';
@@ -7,34 +9,67 @@
 	export let datasource: any[] = [];
 	let columns: IColumnState[] = [];
 
-	$: {
-		if (datasource.length > 0) {
-			columns = Object.keys(datasource[0]).map((columnName) => ({
-				name: columnName,
-				sort: 'None'
-			}));
-		}
-	}
+	onMount(() => {
+		columns = Object.keys(datasource[0]).map((columnName) => ({
+			name: columnName,
+			sort: 'None'
+		}));
+	});
 
 	function columnClick(column: IColumnState) {
 		const columnName = column.name;
 
-		datasource = datasource.sort((a: any, b: any) => {
-			if (a[columnName] > b[columnName]) {
-				return 1;
-			} else if (a[columnName] < b[columnName]) {
-				return -1;
-			}
+		if (column.sort !== 'ASC') {
+			datasource = datasource.sort((a: any, b: any) => {
+				if (a[columnName] > b[columnName]) {
+					return 1;
+				} else if (a[columnName] < b[columnName]) {
+					return -1;
+				}
 
-			return 0;
-		});
+				return 0;
+			});
+
+			columns = columns.map((oryginalColumn: IColumnState) => {
+				if (oryginalColumn.name === column.name) {
+					return { ...oryginalColumn, sort: 'ASC' };
+				}
+
+				return { ...oryginalColumn, sort: 'None' };
+			});
+		} else {
+			datasource = datasource.sort((a: any, b: any) => {
+				if (a[columnName] < b[columnName]) {
+					return 1;
+				} else if (a[columnName] > b[columnName]) {
+					return -1;
+				}
+
+				return 0;
+			});
+
+			columns = columns.map((oryginalColumn: IColumnState) => {
+				if (oryginalColumn.name === column.name) {
+					return { ...oryginalColumn, sort: 'DESC' };
+				}
+
+				return { ...oryginalColumn, sort: 'None' };
+			});
+		}
 	}
 </script>
 
 <table>
 	<thead>
 		{#each columns as column}
-			<th on:click={() => columnClick(column)}>{column.name}</th>
+			<th on:click={() => columnClick(column)}>
+				{column.name}
+				{#if column.sort === 'ASC'}
+					🔺
+				{:else if column.sort === 'DESC'}
+					🔻
+				{/if}
+			</th>
 		{/each}
 	</thead>
 	<tbody>
