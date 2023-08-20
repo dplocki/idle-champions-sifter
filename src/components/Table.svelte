ff91e7e3fb3b50efc5d46b8c63ba1aca3f704102
Ziomioslaw <dawid.plocki@gmail.com>
2023-08-20 16:34:09 +0200

    Display data from database by Table

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -2,6 +2,18 @@
     export let datasource: any[] = [];
 </script>
 
-{#each datasource as datum}
-    <li>{ datum }</li>
-{/each}
+<table>
+    <thead>
+    </thead>
+    <tbody>
+    {#each datasource as datum}
+        <tr>
+        {#each Object.values(datum) as value}
+            <td>{value}</td>
+        {/each}
+        </tr>
+    {/each}
+    </tbody>
+    <tfoot>
+    </tfoot>
+</table>
