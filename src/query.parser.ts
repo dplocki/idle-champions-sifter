4fe4df0879b229937ce388580b0d300eca16e689
Ziomioslaw <dawid.plocki@gmail.com>
2023-09-07 21:22:46 +0200

    Draft for parser

--- src/query.parser.ts
+++ src/query.parser.ts
@@ -1,1 +1,36 @@
-export const lexer = (value: string) => value.split(' ').map((t: string) => t.trim()).filter(t => t !== '');
+export const lexer = (value: string):string[] => value.split(' ').map((t: string) => t.trim()).filter(t => t !== '');
+
+class Token {
+}
+
+class Operator extends Token {
+}
+
+class Value extends Token {
+	constructor(value: string) {
+		super();
+	}
+}
+
+class Column extends Token {
+	constructor(value: string) {
+		super();
+	}
+}
+
+const OPERATORS: { [id: string]: Operator } = {
+	'OR': new Operator(),
+	'AND': new Operator()
+}
+
+export function translateTokens(tokens: string[], columns: string[]): Token[] {
+	return tokens.map((token: string) => {
+		if (token in OPERATORS) {
+			return OPERATORS[token];
+		} else if (token in columns) {
+			return new Column(token)
+		}
+
+		return new Value(token);
+	});
+};
