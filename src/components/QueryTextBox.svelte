ef091b5c991d0a7f98e5e4523c7831aef2506ecf
Ziomioslaw <dawid.plocki@gmail.com>
2023-09-15 22:15:41 +0200

    Introduce the autocomplete code from Svelt examples

--- src/components/QueryTextBox.svelte
+++ src/components/QueryTextBox.svelte
@@ -1,54 +1,162 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import Country from './Country.svelte';
+
+	export const countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre and Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts and Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
 	const dispatch = createEventDispatcher();
 	let query: string = '';
 
-	function clearQuery() {
-		query = '';
+	// function clearQuery() {
+	// 	query = '';
+	// }
+
+	// function keypressed(event: KeyboardEvent) {
+	// 	if (event.key === 'Enter') {
+	// 		dispatch('query', query);
+	// 	} else {
+	// 		showResults(query);
+	// 	}
+	// }
+
+	/* FILTERING countres DATA BASED ON INPUT */
+	let filteredCountries: string[] = [];
+	// $: console.log(filteredCountries)
+
+	const filterCountries = () => {
+		let storageArr: string[] = [];
+		if (inputValue) {
+			countries.forEach((country) => {
+				if (country.toLowerCase().startsWith(inputValue.toLowerCase())) {
+					storageArr = [...storageArr, makeMatchBold(country)];
+				}
+			});
+		}
+		filteredCountries = storageArr;
+	};
+
+	/* HANDLING THE INPUT */
+	let searchInput: HTMLElement; // use with bind:this to focus element
+	let inputValue = '';
+
+	$: if (!inputValue) {
+		filteredCountries = [];
+		hiLiteIndex = null;
 	}
 
-	function keypressed(event: KeyboardEvent) {
-		if (event.key === 'Enter') {
-			dispatch('query', query);
+	const clearInput = () => {
+		inputValue = '';
+		searchInput.focus();
+	};
+
+	const setInputVal = (countryName: string) => {
+		inputValue = removeBold(countryName);
+		filteredCountries = [];
+		hiLiteIndex = null;
+		(document.querySelector('#country-input') as any).focus();
+	};
+
+	const submitValue = () => {
+		if (inputValue) {
+			console.log(`${inputValue} is submitted!`);
+			setTimeout(clearInput, 1000);
+		} else {
+			alert("You didn't type anything.");
 		}
-	}
+	};
+
+	const makeMatchBold = (str: string) => {
+		// replace part of (country name === inputValue) with strong tags
+		let matched = str.substring(0, inputValue.length);
+		let makeBold = `<strong>${matched}</strong>`;
+		let boldedMatch = str.replace(matched, makeBold);
+		return boldedMatch;
+	};
+
+	const removeBold = (str: string) => {
+		//replace < and > all characters between
+		return str.replace(/<(.)*?>/g, '');
+		// return str.replace(/<(strong)>/g, "").replace(/<\/(strong)>/g, "");
+	};
+
+	/* NAVIGATING OVER THE LIST OF COUNTRIES W HIGHLIGHTING */
+	let hiLiteIndex: number | null = null;
+	//$: console.log(hiLiteIndex);
+	$: hiLitedCountry = filteredCountries[hiLiteIndex!];
+
+	const navigateList = (e: KeyboardEvent) => {
+		if (e.key === 'ArrowDown' && hiLiteIndex! <= filteredCountries.length - 1) {
+			hiLiteIndex === null ? (hiLiteIndex = 0) : (hiLiteIndex += 1);
+		} else if (e.key === 'ArrowUp' && hiLiteIndex !== null) {
+			hiLiteIndex === 0 ? (hiLiteIndex = filteredCountries.length - 1) : (hiLiteIndex -= 1);
+		} else if (e.key === 'Enter') {
+			setInputVal(filteredCountries[hiLiteIndex!]);
+		} else {
+			return;
+		}
+	};
 </script>
 
-<div class="input-container">
-	<input type="text" bind:value={query} on:keypress={keypressed} />
-    <button on:click={clearQuery}>Clear</button>
-</div>
+<svelte:window on:keydown={navigateList} />
+
+<form autocomplete="off" on:submit|preventDefault={submitValue}>
+	<div class="autocomplete">
+		<input
+			id="country-input"
+			type="text"
+			placeholder="Search Country Names"
+			bind:this={searchInput}
+			bind:value={inputValue}
+			on:input={filterCountries}
+		/>
+	</div>
+
+	<input type="submit" />
+
+	<!-- FILTERED LIST OF COUNTRIES -->
+	{#if filteredCountries.length > 0}
+		<ul id="autocomplete-items-list">
+			{#each filteredCountries as country, i}
+				<Country
+					itemLabel={country}
+					highlighted={i === hiLiteIndex}
+					on:click={() => setInputVal(country)}
+				/>
+			{/each}
+		</ul>
+	{/if}
+</form>
 
 <style>
-	/* Stylowanie kontenera */
-	.input-container {
-		display: flex;
-		align-items: center;
+	div.autocomplete {
+		/*the container must be positioned relative:*/
+		position: relative;
+		display: inline-block;
+		width: 300px;
 	}
-
-	/* Stylowanie pola tekstowego */
-	.input-container input[type='text'] {
-		padding: 8px;
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		margin-right: 10px;
-		font-size: 14px;
+	input {
+		border: 1px solid transparent;
+		background-color: #f1f1f1;
+		padding: 10px;
+		font-size: 16px;
+		margin: 0;
 	}
-
-	/* Stylowanie przycisku */
-	.input-container button {
-		padding: 8px 12px;
-		background-color: #333;
+	input[type='text'] {
+		background-color: #f1f1f1;
+		width: 100%;
+	}
+	input[type='submit'] {
+		background-color: DodgerBlue;
 		color: #fff;
-		border: none;
-		border-radius: 4px;
-		cursor: pointer;
-		font-size: 14px;
-		transition: background-color 0.3s ease;
 	}
 
-	.input-container button:hover {
-		background-color: #555;
+	#autocomplete-items-list {
+		position: relative;
+		margin: 0;
+		padding: 0;
+		top: 0;
+		width: 297px;
+		border: 1px solid #ddd;
+		background-color: #ddd;
 	}
 </style>
