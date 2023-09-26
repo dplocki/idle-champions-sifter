46562ecd3c43017d562a448c279f7d950a9a97b9
Ziomioslaw <dawid.plocki@gmail.com>
2023-09-26 21:05:45 +0200

    App: Transform the columns names

--- src/app.svelte
+++ src/app.svelte
@@ -5,6 +5,14 @@
 
 	export let database: any[];
 
+	function capitalizeFirstLetter(value: string): string {
+		return value.charAt(0).toUpperCase() + value.slice(1);
+	}
+
+	function transformColumnNameInto(value: string): string {
+		return value.split(' ').map(capitalizeFirstLetter).join('');
+	}
+
 	function newquery(event: CustomEvent<string>) {
 		console.log('newquery', lexer(event.detail));
 	}
@@ -12,7 +20,7 @@
 
 <h1>Idle Champions Sifter</h1>
 
-<QueryTextBox columns={Object.keys(database[0])} />
+<QueryTextBox columns={Object.keys(database[0]).map(transformColumnNameInto)} />
 
 <Table datasource={database} columnsSettings={{ 'Icon': 'img-link' }} />
 
